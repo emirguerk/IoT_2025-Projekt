8efa101932d1d3b055488e457403b75afe1920e3
Timo Ley <timo.ley@tilera.dev>
2025-08-25 21:30:43 +0200

    feat: implement logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 #define DHTTYPE DHT11
 
-const int relayPin = 26;
+const int relayPin = 14;
 const int dhtPin = 16;
 
 // MQTT-Server
@@ -16,7 +16,21 @@ const char* mqtt_server = "broker.hivemq.com";
 const int buttonPin = 18;
 const int touchPin = 34;
 const int ledPin = 26;
-const int ledPin2 = 33;
+
+// MQTT Config
+const char* mqttTopic = "IoT_Emotionsband";
+const char localId = 1;
+
+// Constants
+const char requestCom = 1;
+const char abortCom = 2;
+const char touchStart = 3;
+const char touchEnd = 4;
+
+// State
+int comStatus = 0; // 0: no com 1: com request send 2: comrequest received 3: com established
+int comTimer = 0;
+int ledTimer = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -27,22 +41,30 @@ Heater heater(dhtPin, DHTTYPE, relayPin);
 void catchMessage(char* topic, byte* payload, unsigned int length) {
     Serial.print("Message arrived on topic: ");
     Serial.println(topic);
+    char sender = payload[0];
+    char content = payload[1];
 
-    String message = "";
-    for (int i = 0; i < length; i++) {
-        message += (char)payload[i];
+    if (sender == localId) {
+        return;
     }
-    Serial.print("Message: ");
-    Serial.println(message);
 
-    if (message == "BUTTON_PRESSED") {
-        digitalWrite(ledPin, HIGH);
-        delay(1000);
-        digitalWrite(ledPin, LOW);
-    } else if (message == "TOUCH_DETECTED") {
-        digitalWrite(ledPin2, HIGH);
-        delay(1000);
-        digitalWrite(ledPin2, LOW);
+    Serial.printf("Message: %i\n", content);
+    Serial.printf("Sender: %i\n", sender);
+
+    if (content == requestCom) {
+        if (comStatus == 0) {
+            comStatus = 2;
+            comTimer = 300;
+        } else if (comStatus == 1) {
+            comStatus = 3;
+        }
+    } else if (content == abortCom && comStatus != 1) {
+        comStatus = 0;
+        heater.deactivate();
+    } else if (content == touchStart && comStatus == 3) {
+        heater.activate(30);
+    } else if (content == touchEnd) {
+        heater.deactivate();
     }
 }
 
@@ -52,7 +74,7 @@ void reconnect() {
         Serial.print("MQTT verbinden...");
         if (client.connect("emxrxan")) {
             Serial.println("Verbunden!");
-            client.subscribe("IoT_Emotionsband");
+            client.subscribe(mqttTopic);
         } else {
             Serial.print("Fehler, rc=");
             Serial.print(client.state());
@@ -65,10 +87,9 @@ bool lastButtonState = LOW;
 bool lastTouchState = LOW;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     pinMode(touchPin, INPUT);
-    pinMode(ledPin2, OUTPUT);
     pinMode(ledPin, OUTPUT);
     pinMode(buttonPin, INPUT);
 
@@ -95,6 +116,22 @@ void setup() {
     heater.set_threshold(3);
 }
 
+void ledLoop() {
+    if (comStatus == 0) {
+        digitalWrite(ledPin, LOW);
+    } else if (comStatus == 3) {
+        digitalWrite(ledPin, HIGH);
+    } else if (ledTimer > 0) {
+        digitalWrite(ledPin, LOW);
+    } else if (ledTimer < 0) {
+        digitalWrite(ledPin, HIGH);
+    }
+    ledTimer--;
+    if (ledTimer <= -10) {
+        ledTimer = 10;
+    }
+}
+
 void loop() {
     if (client.connected()) {
         client.loop();
@@ -104,16 +141,48 @@ void loop() {
 
     bool currentButtonState = digitalRead(buttonPin);
     bool currentTouchState = digitalRead(touchPin);
+    char message[3];
+    message[0] = localId;
+    message[2] = 0;
 
     if (currentButtonState == HIGH && lastButtonState == LOW) {
-        client.publish("IoT_Emotionsband", "BUTTON_PRESSED");
+        if (comStatus == 3 || comStatus == 1) {
+            message[1] = abortCom;
+            comStatus = 0;
+            heater.deactivate();
+        } else if (comStatus == 0) {
+            message[1] = requestCom;
+            comStatus = 1;
+            comTimer = 300;
+        } else if (comStatus == 2) {
+            message[1] = requestCom;
+            comStatus = 3;
+        }
+        client.publish(mqttTopic, message);
     }
 
     if (currentTouchState == HIGH && lastTouchState == LOW) {
-        client.publish("IoT_Emotionsband", "TOUCH_DETECTED");
+        message[1] = touchStart;
+        client.publish(mqttTopic, message);
+    }
+
+    if (currentTouchState == LOW && lastTouchState == HIGH) {
+        message[1] = touchEnd;
+        client.publish(mqttTopic, message);
     }
 
     lastButtonState = currentButtonState;
     lastTouchState = currentTouchState;
     heater.loop();
+    delay(100);
+    ledLoop();
+    if (comTimer > 0) {
+        comTimer--;
+    }
+    if (comTimer == 0 && comStatus != 3 && comStatus != 0) {
+        comStatus = 0;
+        message[1] = abortCom;
+        client.publish(mqttTopic, message);
+    }
 }
+
